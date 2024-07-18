82af88162a73a1fb263e92b7e87efee47281b671
Sachin Kumar <skg460@gmail.com>
2024-07-19 04:12:44 +0530

    Updates the end points to give current location of patient

--- src/main/java/com/db/dementiacompass/nfc/NFCController.java
+++ src/main/java/com/db/dementiacompass/nfc/NFCController.java
@@ -1,33 +1,23 @@
 package com.db.dementiacompass.nfc;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class NFCController {
 
-    private final NfcLocation nfcLocation;
-
-    NFCController() {
-        nfcLocation = new NfcLocation();
-        nfcLocation.setShowNotification(false);
-        nfcLocation.setMessage(null);
-    }
+    private String location;
 
     @GetMapping("/notification1")
-    public ResponseEntity<NfcLocation> getNfcLocation() {
-        return new ResponseEntity<>(nfcLocation, HttpStatus.OK);
+    public String getNfcLocation() {
+        System.out.println("API called : " + location);
+        return location;
     }
 
-    @PostMapping("/notification1")
-    public String setNfcLocation(@RequestParam boolean showNotification,
-                               @RequestParam String message) {
-        nfcLocation.setShowNotification(showNotification);
-        nfcLocation.setMessage(message);
+    @GetMapping("/notification")
+    public String setNfcLocation(@RequestParam String message) {
+        location = message;
         return "Location changed : " + message;
     }
 
